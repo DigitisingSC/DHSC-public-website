c8dbcbcfe13fb39bc440b7f44f10361a68506a2f
Endre Soo <endre.soo@tpximpact.com>
2023-02-14 09:28:40 +0000

    Storybook support and example components

---
+++ docroot/themes/custom/dhsc_theme/stories/02-molecules/menus/menu-footer.stories.js
@@ -0,0 +1,12 @@
+import menuFooter from "./menu-footer.twig";
+
+import './../../../../../contrib/localgov_base/css/components/footer.ie11.css';
+import './../../../../../contrib/localgov_base/css/components/footer.css';
+
+export default {
+  title: "Design System/Molecules/Menu Footer",
+};
+
+const Template = () => menuFooter();
+
+export const MenuFooter = Template.bind({});
