1470769d9869b8ea3f9be36b491c49385da4b3d8
Matt Lambert <matthew.lambert@nudgedigital.co.uk>
2023-02-23 14:22:40 +0000

    Theme homepage cards.

--- docroot/themes/custom/dhsc_theme/stories/02-molecules/homepage-card/homepage-card.stories.js
+++ docroot/themes/custom/dhsc_theme/stories/02-molecules/homepage-card/homepage-card.stories.js
@@ -4,12 +4,8 @@ export default {
   title: "Design System/Molecules/Homepage Card",
 };
 
-import homepageCardImage from '../../assets/homepage-card.jpg';
-
-const image = {
-  src: homepageCardImage,
-  alt: 'Digital Social Care',
-};
+import image from '../../assets/homepage-card.jpg';
+const imgTag = `<img src=${image} alt='Digital Social Care'/>`
 
 const Template = ({ image, link, heading, description }) =>
   homepageCard({
@@ -21,7 +17,7 @@ const Template = ({ image, link, heading, description }) =>
 
 export const HomepageCard = Template.bind({});
 HomepageCard.args = {
-  image: image,
+  image: imgTag,
   link: "https://www.digitalsocialcare.co.uk/",
   heading: "Get help using technology at your organisation",
   description: "Description goes here",
