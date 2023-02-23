bad7e27c8643041d4a06a14487e1955a3838e96b
Matt Lambert <matthew.lambert@nudgedigital.co.uk>
2023-02-23 08:42:24 +0000

    Override header css in custom theme.

--- docroot/themes/custom/dhsc_theme/stories/02-molecules/menus/menu-main.stories.js
+++ docroot/themes/custom/dhsc_theme/stories/02-molecules/menus/menu-main.stories.js
@@ -1,9 +1,9 @@
 import menuItem from "./menu-main.twig";
 
-import './../../../../../custom/dhsc_theme/css/components/menu-main.ie11.css';
-import './../../../../../custom/dhsc_theme/css/components/menu-main.css';
 import './../../../../../custom/dhsc_theme/css/layout/header.ie11.css';
 import './../../../../../custom/dhsc_theme/css/layout/header.css';
+import './../../../../../custom/dhsc_theme/css/components/menu-main.ie11.css';
+import './../../../../../custom/dhsc_theme/css/components/menu-main.css';
 
  export default {
    title: "Design System/Molecules/Menu Main",
