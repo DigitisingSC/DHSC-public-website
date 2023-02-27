e78f86dd21382ff7e4370adaae95d233d72e86c2
Matt Lambert <matthew.lambert@nudgedigital.co.uk>
2023-02-27 16:00:08 +0000

    Add banner component and update Drupal paragraph template.

--- docroot/themes/custom/dhsc_theme/stories/01-atoms/banner/banner.stories.js
+++ docroot/themes/custom/dhsc_theme/stories/01-atoms/banner/banner.stories.js
@@ -1,5 +1,7 @@
 import banner from "./banner.twig";
 
+import './../../../../../custom/dhsc_theme/css/components/banner.css';
+
 export default {
   title: "Design System/Atoms/Banner",
 };
@@ -7,12 +9,18 @@ export default {
 import image from '../../../assets/images/banner.jpg';
 const imgTag = `<img src=${image} alt='Digital Social Care Banner'/>`
 
-const Template = ({ image }) =>
+const Template = ({ image, url, heading, text }) =>
   banner({
     image,
+    url,
+    heading,
+    text,
   });
 
-export const banner = Template.bind({});
-banner.args = {
+export const bannerHTML = Template.bind({});
+bannerHTML.args = {
   image: imgTag,
+  url: "https://www.digitalsocialcare.co.uk/",
+  heading: "Banner content",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
 };
