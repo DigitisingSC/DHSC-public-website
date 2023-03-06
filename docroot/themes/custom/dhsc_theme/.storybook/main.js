a46d77a9f7feaa9f4f0b99d58a44b16b1002b3ec
Endre Soo <endre.soo@tpximpact.com>
2023-03-06 13:28:39 +0000

    Add gulp and SCSS support for storybook

--- docroot/themes/custom/dhsc_theme/.storybook/main.js
+++ docroot/themes/custom/dhsc_theme/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../stories/**/*.stories.@(js|mdx)'],
 	addons: [
@@ -5,6 +7,7 @@ module.exports = {
 		"@storybook/addon-essentials",
 		"@storybook/addon-docs",
 		"@storybook/addon-viewport",
+		"storybook-addon-sass-postcss",
 	],
 	staticDirs: ['../stories/assets'],
 	webpackFinal: async (config, { configType }) => {
@@ -12,6 +15,31 @@ module.exports = {
 			test: /\.twig$/,
 			use: "twigjs-loader",
 		});
+
+		// config.module.rules = config.module.rules.filter((rule) => {
+		// 	console.log('INDEX: ', rule.test, rule.test.toString() === '/\\.css$/')
+		// 	if(rule.test.toString() == '/\\.(css|scss|sass)$/i') {
+		// 		console.log("RULE", rule.use)
+		// 	}
+
+		// 	return rule;
+		// })
+
+		config.module.rules = config.module.rules.filter((rule) => rule.test.toString() !== '/\\.css$/')
+
+		config.module.rules.push({
+			test: /\.scss$/,
+			use: [
+				{
+					loader: "sass-loader",
+					options: {
+						sassOptions: {
+							quietDeps: true,
+						},
+					},
+				},
+			],
+		})
 
 		return config;
 	}
--- docroot/themes/custom/dhsc_theme/gulpfile.js
+++ docroot/themes/custom/dhsc_theme/gulpfile.js
@@ -1,14 +1,36 @@
-'use strict';
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+"use strict";
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const babel = require("gulp-babel");
+const minify = require("gulp-minify");
 
 
-gulp.task('sass', function () {
-	return gulp.src('./stories/**/*.scss')
-	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-	.pipe(gulp.dest('./dist/styles/'))
+gulp.task("sass", function () {
+  return gulp
+    .src("./stories/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(gulp.dest("./dist/"));
 });
 
-gulp.task('sass:watch', function () {
-	gulp.watch('./stories/**/*.scss', ['sass']);
+gulp.task("js", () => {
+  return gulp
+    .src(["stories/**/*.js", "!stories/**/*.stories.js"])
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
+    .pipe(
+      minify({
+        ext: {
+          min: ".min.js",
+        },
+      })
+    )
+    .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task("watch:all", async function () {
+  gulp.watch("./stories/**/*.scss", gulp.series("sass"));
+  gulp.watch(["stories/**/*.js", "!stories/**/*.stories.js"], gulp.series("js"));
 });
