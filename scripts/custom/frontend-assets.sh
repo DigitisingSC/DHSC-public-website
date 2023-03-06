9b8f43ccc2b821a2514bbf0042d6103cffb7e6df
Endre Soo <endre.soo@tpximpact.com>
2023-03-06 16:35:52 +0000

    FE assets script update

--- scripts/custom/frontend-assets.sh
+++ scripts/custom/frontend-assets.sh
@@ -7,4 +7,4 @@ echo "Building frontend assets."
 npm i --prefix docroot/themes/custom/dhsc_theme
 npm run build --prefix docroot/themes/custom/dhsc_theme
 
-echo "No frontend assets"
+echo "Frontend assets built."
