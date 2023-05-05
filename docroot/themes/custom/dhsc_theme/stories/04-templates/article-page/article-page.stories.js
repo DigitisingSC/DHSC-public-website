d08be0e47c89d69587dc62b899ce8032e2ac79a6
Matt Lambert <matthew.lambert@tpximpact.com>
2023-05-05 14:40:02 +0100

    (feat) : (DHSC-92) Add initial article page template, story.

---
+++ docroot/themes/custom/dhsc_theme/stories/04-templates/article-page/article-page.stories.js
@@ -0,0 +1,17 @@
+import articlePage from "./article-page.twig";
+
+export default {
+  title: "Design System/Templates/Article page",
+};
+
+const Template = ({ article_details, content }) =>
+  articlePage({
+    article_details,
+    content
+  });
+
+export const ArticlePage = Template.bind({});
+ArticlePage.args = {
+  article_details: "article details..",
+  content: "article page.."
+};
