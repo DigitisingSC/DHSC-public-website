21e8a87d4809f0ebfe779a5e55583033b2cbf057
Cedric Hillion <cedric.hillion@manifesto.co.uk>
2023-05-12 16:47:14 +0100

    feat: composing the header in storybook

--- docroot/themes/custom/dhsc_theme/stories/03-organisms/header/header.stories.js
+++ docroot/themes/custom/dhsc_theme/stories/03-organisms/header/header.stories.js
@@ -1,15 +1,22 @@
+import React from 'react';
 import header from "./header.twig";
-
+import { Branding } from "../../02-molecules/branding/branding.stories";
+import BrandingTwig from "../../02-molecules/branding/branding.twig";
 export default {
   title: "Design System/Organisms/Header",
+  content: Branding
 };
 
-const Template = ({ content}) =>
+const Template = ({ content }) =>
   header({
     content
   });
 
+const BrandTemplate = (args) => BrandingTwig({
+  ...Branding.args
+});
+
 export const Header = Template.bind({});
 Header.args = {
-  content: 'Content of the region'
+  content: BrandTemplate
 }
