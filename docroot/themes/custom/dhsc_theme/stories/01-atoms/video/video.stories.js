7c204e1a239496a4301bcf2d4b1ff8eaa3a159a1
Matt Lambert <matthew.lambert@tpximpact.com>
2023-05-22 14:31:12 +0100

    (feat) : (DHSC-94) video component updates.

--- docroot/themes/custom/dhsc_theme/stories/01-atoms/video/video.stories.js
+++ docroot/themes/custom/dhsc_theme/stories/01-atoms/video/video.stories.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import DrupalAttribute from '../../../.storybook/drupalAttributes';
-import video from "./video.twig";
+import videoTwig from "./video.twig";
+import videoBehaviour from "./video.js";
+
 
 export default {
   title: "Design System/Atoms/Video",
 };
 
-const Template = ({ title }) =>
-  video({
-    title,
+const Template = ({iframe, media_thumbnail, media_thumbnail_legacy}) =>
+  videoTwig({
+    iframe,
+    media_thumbnail,
+    media_thumbnail_legacy,
   });
 
-export const videoHTML = Template.bind({});
-videoHTML.args = {
-  title: "Video title..",
+export const video = Template.bind({});
+video.args = {
+  iframe: "<iframe class='' frameborder='0' allow='autoplay' data-src='https://www.youtube.com/embed/JkaxUblCGz0?autoplay=1'></iframe>",
+  media_thumbnail: "//i.ytimg.com/vi_webp/JkaxUblCGz0/maxresdefault.webp",
+  media_thumbnail_legacy: "//i.ytimg.com/vi/JkaxUblCGz0/maxresdefault.jpg",
 };
