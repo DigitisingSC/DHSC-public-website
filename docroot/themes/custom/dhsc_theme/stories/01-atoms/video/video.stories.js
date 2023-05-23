e862576002179c7887fd86c0760bd422a811ebcd
Cedric Hillion <cedric.hillion@manifesto.co.uk>
2023-05-23 14:04:37 +0100

    feat: fixes small issue with DrupalAttributes in Video component

--- docroot/themes/custom/dhsc_theme/stories/01-atoms/video/video.stories.js
+++ docroot/themes/custom/dhsc_theme/stories/01-atoms/video/video.stories.js
@@ -2,15 +2,16 @@ import React from 'react';
 import DrupalAttribute from '../../../.storybook/drupalAttributes';
 import videoTwig from "./video.twig";
 import videoBehaviour from "./video.js";
-import './css/video.scss'
+import './video.scss';
 
 
 export default {
   title: "Design System/Atoms/Video",
 };
 
-const Template = ({iframe, media_thumbnail, media_thumbnail_legacy}) =>
+const Template = ({attributes, iframe, media_thumbnail, media_thumbnail_legacy}) =>
   videoTwig({
+    attributes,
     iframe,
     media_thumbnail,
     media_thumbnail_legacy,
@@ -18,6 +19,7 @@ const Template = ({iframe, media_thumbnail, media_thumbnail_legacy}) =>
 
 export const video = Template.bind({});
 video.args = {
+  attributes: new DrupalAttribute(),
   iframe: "<iframe class='' frameborder='0' allow='autoplay' data-src='https://www.youtube.com/embed/JkaxUblCGz0?autoplay=1'></iframe>",
   media_thumbnail: "//i.ytimg.com/vi_webp/JkaxUblCGz0/maxresdefault.webp",
   media_thumbnail_legacy: "//i.ytimg.com/vi/JkaxUblCGz0/maxresdefault.jpg",
