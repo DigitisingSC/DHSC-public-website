a2c9e71c2bce3095023db2713badbec5dfdf922a
Cedric Hillion <cedric.hillion@manifesto.co.uk>
2023-05-25 16:18:02 +0100

    feat: (DHSC-111) tweaks for Card collection and article, event and case study components

--- docroot/themes/custom/dhsc_theme/stories/03-organisms/card-collection/card-collection.stories.js
+++ docroot/themes/custom/dhsc_theme/stories/03-organisms/card-collection/card-collection.stories.js
@@ -1,20 +1,19 @@
-import DrupalAttributes from '../../../.storybook/drupalAttributes';
 import React from 'react';
+import DrupalAttributes from '../../../.storybook/drupalAttributes';
 import cardCollectionTwig from "./card-collection.twig";
 
 import { cardArticle } from "../../02-molecules/card/card.stories.js";
+import cardArticleTwig from "../../02-molecules/card/card--article.twig";
 import { cardEvent } from "../../02-molecules/card/card.stories.js";
-import { cardCaseStudy } from "../../02-molecules/card/card.stories.js";
-
-import cardArticeTwig from "../../02-molecules/card/card--article.twig";
 import cardEventTwig from "../../02-molecules/card/card--event.twig";
+import { cardCaseStudy } from "../../02-molecules/card/card.stories.js";
 import cardCaseStudyTwig from "../../02-molecules/card/card--casestudy.twig";
 
 export default {
   title: "Design System/Organisms/Card Collection",
 };
 
-const cardArticleTemplate = (args) => cardArticeTwig({
+const cardArticleTemplate = (args) => cardArticleTwig({
   ...cardArticle.args,
 });
 
@@ -22,12 +21,13 @@ const cardEventTemplate = (args) => cardEventTwig({
   ...cardEvent.args
 });
 
-const cardCasestudyTemplate = (args) => cardCaseStudyTwig({
+const cardCaseStudyTemplate = (args) => cardCaseStudyTwig({
   ...cardCaseStudy.args
 });
 
-const cardCollectionTemplate = ({ title, items }) =>
+const cardCollectionTemplate = ({ attributes, title, items }) =>
   cardCollectionTwig({
+    attributes,
     title,
     items
   });
@@ -35,6 +35,7 @@ const cardCollectionTemplate = ({ title, items }) =>
 export const cardCollection = cardCollectionTemplate.bind({});
 
 cardCollection.args = {
+  attributes: new DrupalAttributes(),
   title: 'Card Collection',
-  items: { cardArticleTemplate, cardEventTemplate, cardCasestudyTemplate },
+  items: { cardArticleTemplate, cardEventTemplate, cardCaseStudyTemplate },
 }
