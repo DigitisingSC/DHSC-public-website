98e50a35c99166469bdc559e9a5da96282243bc4
Cedric Hillion <cedric.hillion@manifesto.co.uk>
2023-05-26 15:20:55 +0100

    feat: fixes a recurrent issue with video component inception and SVG icon import

--- docroot/themes/custom/dhsc_theme/stories/01-atoms/video/video.stories.js
+++ docroot/themes/custom/dhsc_theme/stories/01-atoms/video/video.stories.js
@@ -2,19 +2,25 @@ import React from 'react';
 import DrupalAttributes from '../../../.storybook/drupalAttributes';
 import videoTwig from "./video.twig";
 import videoBehaviour from "./video.js";
+import { svgIcon } from '../svg/svg.stories';
 import './video.scss';
 
 export default {
   title: "Design System/Atoms/Video",
 };
 
-const Template = ({attributes, iframe, media_thumbnail, media_thumbnail_legacy}) =>
+const Template = ({attributes, iframe, media_thumbnail, media_thumbnail_legacy, icon }) =>
   videoTwig({
     attributes,
     iframe,
     media_thumbnail,
     media_thumbnail_legacy,
+    icon
   });
+
+const svgIconTemplate = (args) => svgIcon({
+  ...svgIcon.args
+});
 
 export const video = Template.bind({});
 video.args = {
@@ -22,4 +28,5 @@ video.args = {
   iframe: "<iframe class='' frameborder='0' allow='autoplay' data-src='https://www.youtube.com/embed/JkaxUblCGz0?autoplay=1'></iframe>",
   media_thumbnail: "//i.ytimg.com/vi_webp/JkaxUblCGz0/maxresdefault.webp",
   media_thumbnail_legacy: "//i.ytimg.com/vi/JkaxUblCGz0/maxresdefault.jpg",
+  icon: svgIconTemplate,
 };
