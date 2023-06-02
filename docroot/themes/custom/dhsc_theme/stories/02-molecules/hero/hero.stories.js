cf8c9cf537d01a833539a1a7165d60f76703d039
Nicolas Borda <nicolas.borda@manifesto.co.uk>
2023-06-02 09:41:59 +0200

    feat: (DHSC-148) add hero component

---
+++ docroot/themes/custom/dhsc_theme/stories/02-molecules/hero/hero.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import DrupalAttributes from '../../../.storybook/drupalAttributes';
+import hero from "./hero.twig";
+import "./hero.scss";
+
+export default {
+  title: "Design System/Molecules/Hero",
+};
+
+import image from '../../../assets/images/hero.png';
+const imgTag = `<img src=${image} alt='Digital Social Care Banner'/>`;
+
+const Template = ({ image, url, heading, text }) =>
+  hero({
+    image,
+    url,
+    heading,
+    text,
+  });
+
+export const Hero = Template.bind({});
+Hero.args = {
+  image: imgTag,
+  heading: "Supporting your digital social care journey",
+  text: "<p>Provide better and safer care by using digital ways of working</p><p>Watch our <a href='#'>video</a> to find out more</p><p>Provide better and safer care by using digital ways of working</p><p>Watch our <a href='#'>video</a> to find out more</p><p>Provide better and safer care by using digital ways of working</p><p>Watch our <a href='#'>video</a> to find out more</p><p>Provide better and safer care by using digital ways of working</p><p>Watch our <a href='#'>video</a> to find out more</p>",
+};
