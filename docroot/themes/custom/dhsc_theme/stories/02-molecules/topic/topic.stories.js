95ded5dd74705b5c44a39e2fbf1c612cb9f31fb6
Nicolas Borda <nicolas.borda@manifesto.co.uk>
2023-06-26 08:55:50 +0200

    fix: (DHSC-165) adjust icon, hover, and focus states on topics

--- docroot/themes/custom/dhsc_theme/stories/02-molecules/topic/topic.stories.js
+++ docroot/themes/custom/dhsc_theme/stories/02-molecules/topic/topic.stories.js
@@ -2,23 +2,31 @@ import React from 'react';
 import DrupalAttributes from '../../../.storybook/drupalAttributes';
 import './topic.scss';
 import topicTwig from "./topic.twig";
+import { svgIcon } from '../../01-atoms/svg/svg.stories';
 
 export default {
   title: "Design System/Molecules/Topic",
 };
 
-const Template = ({ link, heading, description, attributes }) =>
+const svgIconTemplate = (args) => svgIcon({
+  ...svgIcon.args
+});
+
+const Template = ({ link, heading, description, attributes, icon }) =>
   topicTwig({
     link,
     heading,
     description,
-    attributes
+    attributes,
+    icon
   });
+
 
 export const Topic = Template.bind({});
 Topic.args = {
   link: "https://www.digitalsocialcare.co.uk/",
   heading: "Get help using technology at your organisation",
   description: "Learn how to get connected, use secure email, use mobile devices and get digital social care records",
-  attributes: new DrupalAttributes()
+  attributes: new DrupalAttributes(),
+  icon: svgIconTemplate,
 };
