b19618fe6e1e4fd71356e0233449e023f951de39
Endre Soo <endre.soo@tpximpact.com>
2023-07-05 15:38:35 +0100

    Add check to back back button because it's causing JS errors

--- docroot/themes/custom/dhsc_theme/stories/02-molecules/back-link/back-link.js
+++ docroot/themes/custom/dhsc_theme/stories/02-molecules/back-link/back-link.js
@@ -3,10 +3,12 @@ if (typeof Drupal !== 'undefined') {
   Drupal.behaviors.back_link = {
     attach: function (context) {
       const back_link = context.querySelector('.m-back-link');
-      back_link.addEventListener('click', function (event) {
-        event.preventDefault();
-        history.back();
-      });
+      if (back_link) {
+        back_link.addEventListener('click', function (event) {
+          event.preventDefault();
+          history.back();
+        });
+      }
     }
   }
 }
