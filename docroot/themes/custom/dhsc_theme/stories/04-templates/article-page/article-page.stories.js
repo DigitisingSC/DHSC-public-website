5142e398e23cd8cf9ca2ea94d36c5edd3b8201a8
Endre Soo <endre.soo@tpximpact.com>
2023-07-13 16:19:22 +0100

    Article page styling

--- docroot/themes/custom/dhsc_theme/stories/04-templates/article-page/article-page.stories.js
+++ docroot/themes/custom/dhsc_theme/stories/04-templates/article-page/article-page.stories.js
@@ -6,8 +6,9 @@ export default {
   title: "Design System/Templates/Article page",
 };
 
-const Template = ({ title, article_details, content }) =>
+const Template = ({ attributes, title, article_details, content }) =>
   articlePage({
+    attributes,
     title,
     article_details,
     content
@@ -15,7 +16,19 @@ const Template = ({ title, article_details, content }) =>
 
 export const ArticlePage = Template.bind({});
 ArticlePage.args = {
+  attributes: new DrupalAttributes(),
   title: "Article page",
-  article_details: "article details..",
-  content: "Article page content.."
+  article_details: {
+    author: "Jane Doe",
+    date: '2014-07-02',
+  },
+  content: `Vestibulum turpis sem, aliquet eget, lobortis pellentesque, rutrum eu, nisl. Donec mollis hendrerit risus. Praesent blandit laoreet nibh. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Phasellus gravida semper nisi.
+
+  Duis lobortis massa imperdiet quam. Curabitur vestibulum aliquam leo. Vestibulum ullamcorper mauris at ligula. Ut non enim eleifend felis pretium feugiat. Nullam tincidunt adipiscing enim.
+
+  Vivamus consectetuer hendrerit lacus. Proin viverra, ligula sit amet ultrices semper, ligula arcu tristique sapien, a accumsan nisi mauris ac eros. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Aliquam eu nunc. Etiam rhoncus.
+
+  Nunc nulla. Vivamus laoreet. Nullam quis ante. Pellentesque commodo eros a enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+
+  In hac habitasse platea dictumst. Ut varius tincidunt libero. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Aenean ut eros et nisl sagittis vestibulum.`
 };
