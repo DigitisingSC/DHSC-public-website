e9932ed74dd5c8e858612b25c235bcc0525af7f7
Endre Soo <endre.soo@tpximpact.com>
2023-08-03 16:47:38 +0100

    DHSC-188 - Remive hide breadcrumbs JS logic

--- docroot/themes/custom/dhsc_theme/stories/02-molecules/webform/webform.js
+++ docroot/themes/custom/dhsc_theme/stories/02-molecules/webform/webform.js
@@ -1,16 +1,6 @@
 // Only run Drupal code if Drupal is defined.
 if (typeof Drupal !== 'undefined') {
 
-  Drupal.behaviors.multi_step_webform_breadcrumbs = {
-    attach: function (context) {
-      // Find webform with class exists
-      if (context.querySelector('form.m-webform__submission-form--hide-breadcrumbs')) {
-        // If not hide breadcrumbs
-        context.querySelector('.o-breadcrumb-region').style.display = 'none';
-      }
-    }
-  }
-
   Drupal.behaviors.multi_step_webform_meta_title = {
     attach: function (context) {
       // Find replacement title for webform page
