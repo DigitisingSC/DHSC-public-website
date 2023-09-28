f734173c4f0a10b9a9fa623ecf80c80133c3030e
Matt Lambert <matthew.lambert@tpximpact.com>
2023-09-28 12:38:41 +0100

    (feat) : (DHSC-259) add insert table option to relevant text formats, update styles and add responsive table filter module.

--- docroot/themes/custom/dhsc_theme/tailwind.config.js
+++ docroot/themes/custom/dhsc_theme/tailwind.config.js
@@ -228,6 +228,28 @@ module.exports = {
                 },
               },
             },
+            // Tables
+            {
+              table: {
+                th:{
+                  fontSize: '1.1875rem',
+                  lineHeight: '1.75rem',
+                  verticalAlign: 'top',
+                  paddingTop: '0.9375rem',
+                  paddingBottom: '0.9375rem',
+                  borderBottomWidth: '0.125rem',
+                },
+                td: {
+                  fontSize: '1.1875rem',
+                  lineHeight: '1.625rem',
+                  paddingTop: '0.9375rem',
+                  paddingBottom: '0.9375rem',
+                  paddingLeft: '0.25rem',
+                  paddingRight: '0.25rem',
+                  borderBottomWidth: '1px',
+                },
+              }
+            },
           ],
         },
       }),
