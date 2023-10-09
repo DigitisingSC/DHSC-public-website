ad3a4e17c9e6593e774e710f27900b5076064a84
Matt Lambert <matthew.lambert@tpximpact.com>
2023-10-09 16:05:42 +0100

    (feat) : (DHSC-188) Back link component updates.

--- docroot/themes/custom/dhsc_theme/stories/02-molecules/back-link/back-link.stories.js
+++ docroot/themes/custom/dhsc_theme/stories/02-molecules/back-link/back-link.stories.js
@@ -14,11 +14,14 @@ const svgBackLinkTemplate = (args) => svgIconTwig({
   icon: 'chevron-left',
 });
 
-const Template = ({ attributes, label, icon }) =>
+const Template = ({ attributes, label, icon, text, link, variant }) =>
   backLinkTwig({
    attributes,
    label,
    icon,
+   text,
+   link,
+   variant
   });
 
 export const backLink = Template.bind({});
@@ -26,4 +29,21 @@ backLink.args = {
   attributes: new DrupalAttributes(),
   label: 'Back',
   icon: svgBackLinkTemplate,
+};
+
+export const backLinkForm = Template.bind({});
+backLinkForm.args = {
+  attributes: new DrupalAttributes(),
+  label: 'Back',
+  icon: svgBackLinkTemplate,
+  link: '#',
+  variant: 'form'
+};
+
+export const backLinkWithText = Template.bind({});
+backLinkWithText.args = {
+  attributes: new DrupalAttributes(),
+  text: 'Get help with technology',
+  link: '/get-help-set-and-use-technology',
+  variant: 'text'
 };
