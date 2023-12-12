be808605ec023a82b2f9303fc86d00d34c952eda
Matt Lambert <matthew.lambert@tpximpact.com>
2023-12-12 11:33:05 +0000

    feat : (DHSCSM-13) remove site wide alerts module and change banner implementation to custom block plugin.

---
+++ docroot/modules/custom/dhsc_site/src/Plugin/Block/SiteWideAlert.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\dhsc_site\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a sitewide alter block with a text input field.
+ *
+ * @Block(
+ *   id = "sitewide_alert",
+ *   admin_label = @Translation("Sitewide alert"),
+ *   category = @Translation("DHSC Site")
+ * )
+ */
+class SiteWideAlert extends BlockBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+    $text = !empty($this->configuration['alert_text']['value']) ?
+    $this->configuration['alert_text']['value'] : '';
+
+    return [
+      '#theme' => 'alert_message',
+      '#alert_text' => $this->t($text),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state)
+  {
+    $form['alert_text'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Alert text'),
+      '#default_value' => $this->configuration['alert_text']['value'],
+      '#format' => $this->configuration['alert_text']['format'] ?? 'basic_html',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state)
+  {
+    $this->configuration['alert_text'] = $form_state->getValue('alert_text');
+  }
+}
