f11466f6a5ed9354e1c915d2b353886f1a03d6e3
Matt Lambert <matthew.lambert@tpximpact.com>
2023-12-12 13:15:47 +0000

    feat : (DHSCSM-13) formatting and custom cache tag.

--- docroot/modules/custom/dhsc_site/src/Plugin/Block/SiteWideAlert.php
+++ docroot/modules/custom/dhsc_site/src/Plugin/Block/SiteWideAlert.php
@@ -3,6 +3,7 @@
 namespace Drupal\dhsc_site\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
@@ -20,22 +21,23 @@ class SiteWideAlert extends BlockBase
   /**
    * {@inheritdoc}
    */
-  public function build()
-  {
+  public function build() {
     $text = !empty($this->configuration['alert_text']['value']) ?
     $this->configuration['alert_text']['value'] : '';
 
     return [
       '#theme' => 'alert_message',
       '#alert_text' => $this->t($text),
+      '#cache' => [
+        'tags' => ['alert_text_value'],
+      ],
     ];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function blockForm($form, FormStateInterface $form_state)
-  {
+  public function blockForm($form, FormStateInterface $form_state) {
     $form['alert_text'] = [
       '#type' => 'text_format',
       '#title' => $this->t('Alert text'),
@@ -50,8 +52,10 @@ class SiteWideAlert extends BlockBase
   /**
    * {@inheritdoc}
    */
-  public function blockSubmit($form, FormStateInterface $form_state)
-  {
+  public function blockSubmit($form, FormStateInterface $form_state) {
     $this->configuration['alert_text'] = $form_state->getValue('alert_text');
+
+    // Invalidate cache when the alert text field value is updated.
+    Cache::invalidateTags(['alert_text_value']);
   }
 }
