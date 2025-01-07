c61ec9a9bdf9d74a9fd2ea5020102065582ddafa
Luke Whitmore <33448330+lukewhitmore@users.noreply.github.com>
2025-01-07 16:09:19 +0000

    task: (JDLA-32) Addressed missing parameter and return value descriptions in doc comments and resolved issues with \Drupal and t() calls by replacing them with dependency injection.

--- docroot/modules/custom/dhsc_result_viewer/src/Controller/ResultSummarySelfAssessmentController.php
+++ docroot/modules/custom/dhsc_result_viewer/src/Controller/ResultSummarySelfAssessmentController.php
@@ -5,15 +5,19 @@ namespace Drupal\dhsc_result_viewer\Controller;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\Core\Mail\MailManagerInterface;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Render\Markup;
+use Drupal\Core\TempStore\PrivateTempStoreFactory;
 use Drupal\Core\Url;
 use Drupal\dhsc_result_viewer\Form\ResultSummaryForm;
 use Drupal\dhsc_result_viewer\SelfAssessmentInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Class ResultSummarySelfAssessmentController.
@@ -65,6 +69,34 @@ class ResultSummarySelfAssessmentController extends ControllerBase {
   protected $messenger;
 
   /**
+   * The TempStore factory service.
+   *
+   * @var \Drupal\Core\TempStore\PrivateTempStoreFactory
+   */
+  protected $tempStoreFactory;
+
+  /**
+   * The form builder service.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * The request stack service.
+   *
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
+   * The logger factory service.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelFactoryInterface
+   */
+  protected $loggerFactory;
+
+  /**
    * ResultSummaryController constructor.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
@@ -73,6 +105,20 @@ class ResultSummarySelfAssessmentController extends ControllerBase {
    *   The configuration factory.
    * @param \Drupal\dhsc_self_assessment_result_viewer\SelfAssessmentInterface $result_viewer
    *   ResultViewer service.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager service.
+   * @param \Drupal\Core\Mail\MailManagerInterface $mail_manager
+   *   The mail manager service.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The Messenger service.
+   * @param \Drupal\Core\TempStore\PrivateTempStoreFactory $temp_store_factory
+   *   The TempStore factory service.
+   * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
+   *   The form builder service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack service.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   *   The logger factory service.
    */
   public function __construct(
     EntityTypeManagerInterface $entity_type_manager,
@@ -81,6 +127,10 @@ class ResultSummarySelfAssessmentController extends ControllerBase {
     LanguageManagerInterface $language_manager,
     MailManagerInterface $mail_manager,
     MessengerInterface $messenger,
+    PrivateTempStoreFactory $temp_store_factory,
+    FormBuilderInterface $form_builder,
+    RequestStack $request_stack,
+    LoggerChannelFactoryInterface $logger_factory,
   ) {
     $this->entityTypeManager = $entity_type_manager;
     $this->configFactory = $config_factory;
@@ -88,6 +138,10 @@ class ResultSummarySelfAssessmentController extends ControllerBase {
     $this->languageManager = $language_manager;
     $this->mailManager = $mail_manager;
     $this->messenger = $messenger;
+    $this->tempStoreFactory = $temp_store_factory;
+    $this->formBuilder = $form_builder;
+    $this->requestStack = $request_stack;
+    $this->loggerFactory = $logger_factory;
   }
 
   /**
@@ -101,6 +155,10 @@ class ResultSummarySelfAssessmentController extends ControllerBase {
       $container->get('language_manager'),
       $container->get('plugin.manager.mail'),
       $container->get('messenger'),
+      $container->get('tempstore.private'),
+      $container->get('form_builder'),
+      $container->get('request_stack'),
+      $container->get('logger.factory')
     );
   }
 
@@ -108,7 +166,7 @@ class ResultSummarySelfAssessmentController extends ControllerBase {
    * Get submission results.
    *
    * @return mixed
-   *   Resurn submission results.
+   *   Return submission results.
    */
   public function getResults($submission) {
     /** @var \Drupal\webform\WebformSubmissionInterface $submission */
@@ -137,14 +195,14 @@ class ResultSummarySelfAssessmentController extends ControllerBase {
     $webform = $submission->getWebform();
 
     // Extract unique submission token value from URL.
-    if ($submission_token = \Drupal::request()->query->get('token')) {
+    if ($submission_token = $this->requestStack->getCurrentRequest()->query->get('token')) {
       $webform_url = Url::fromRoute('entity.webform.canonical', ['webform' => $webform->id()])->toString();
       $submission_url = Url::fromUserInput($webform_url, ['query' => ['token' => $submission_token]])->toString();
     }
 
     if ($result = $this->getResults($submission)) {
 
-      $tempStore = \Drupal::service('tempstore.private')->get('dhsc_result_viewer');
+      $tempStore = $this->tempStoreFactory->get('dhsc_result_viewer');
 
       // Save result data in tempstore for email result behaviour.
       $tempStore->set('self_assessment_result_data', $result);
@@ -155,8 +213,8 @@ class ResultSummarySelfAssessmentController extends ControllerBase {
         '#title' => $config->get('title') ? $config->get('title') : NULL,
         '#summary' => $config->get('sa_result_summary') ? $config->get('sa_result_summary') : NULL,
         '#result' => $result,
-        '#submission_url' => isset($submission_url) ? $submission_url : NULL,
-        '#email_form' => \Drupal::formBuilder()->getForm('Drupal\dhsc_result_viewer\Form\ResultEmailForm'),
+        '#submission_url' => $submission_url ?? NULL,
+        '#email_form' => $this->formBuilder->getForm('Drupal\dhsc_result_viewer\Form\ResultEmailForm'),
       ];
     }
     else {
@@ -175,25 +233,28 @@ class ResultSummarySelfAssessmentController extends ControllerBase {
    * Email submission results.
    *
    * @param string $email
+   *   The email address.
+   * @param string $token
+   *   The token.
    *
-   * @return void
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   *   The response.
    */
   public function sendResultEmail($email, $token) {
-    $tempStore = \Drupal::service('tempstore.private')->get('dhsc_result_viewer');
+    $tempStore = $this->tempStoreFactory->get('dhsc_result_viewer');
 
     // Get saved result data from tempstore.
     $results = $tempStore->get('self_assessment_result_data');
 
     if (!empty($results)) {
-
       $result = $this->buildEmail($email, $results);
 
       if (!$result['result']) {
-        $message = t('There was a problem sending self assessment result email to @email', ['@email' => $email]);
-        \Drupal::logger('Error in email sending')->error($message);
+        $message = $this->t('There was a problem sending self assessment result email to @email', ['@email' => $email]);
+        $this->loggerFactory->get('Error in email sending')->error($message);
       }
       else {
-        \Drupal::logger('dhsc_result_viewer')->notice('self assessment result email sent to ' . $email);
+        $this->loggerFactory->get('dhsc_result_viewer')->notice('self assessment result email sent to ' . $email);
       }
 
       $submission_url = Url::fromRoute(
@@ -205,26 +266,30 @@ class ResultSummarySelfAssessmentController extends ControllerBase {
         new RedirectResponse($submission_url) :
         new RedirectResponse('/');
 
-      $this->messenger->addStatus(t('Email sent'));
+      $this->messenger->addStatus($this->t('Email sent'));
 
       return $response;
     }
-    $this->messenger->addError(t('Unable to send email'));
+    $this->messenger->addError($this->t('Unable to send email'));
   }
 
   /**
    * Construct results email.
    *
    * @param string $email
+   *   The email address.
+   * @param array $results
+   *   Results array.
    *
    * @return array
+   *   Array containing all details of the message.
    */
-  public function buildEmail($email, $results) {
+  public function buildEmail(string $email, array $results) {
     $module = 'dhsc_result_viewer';
     $key = 'email_result';
     $to = $email;
     $langcode = $this->languageManager->getDefaultLanguage()->getId();
-    $params['subject'] = t('Get started with email recommendations');
+    $params['subject'] = $this->t('Get started with email recommendations');
 
     $result_items = '';
     if ($results) {
